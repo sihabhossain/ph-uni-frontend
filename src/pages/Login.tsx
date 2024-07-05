9210cb03d11951ef2cfffcb23971c64447ef922e
Sihab Hossain <sihabhossain380@gmail.com>
2024-07-05 23:45:55 +0600

    persist added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,47 @@
-
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
-  return (
-    <div>
-      This is login page
-    </div>
-  )
-}
+  const dispatch = useAppDispatch();
 
-export default Login
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "0001",
+      password: "admin12345",
+    },
+  });
+
+  const [login, { error }] = useLoginMutation();
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    console.log(user);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
+};
+
+export default Login;
---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type TAuthState = {
+  user: null | object;
+  token: null | string;
+};
+
+const initialState: TAuthState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+
+      state.user = user;
+      state.token = token;
+    },
+
+    logOut: (state) => {
+      state.token = null;
+      state.user = null;
+    },
+  },
+});
+
+export const { setUser, logOut } = authSlice.actions;
+export default authSlice.reducer;
